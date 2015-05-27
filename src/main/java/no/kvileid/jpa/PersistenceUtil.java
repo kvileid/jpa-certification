92ef9f4d3e3a7e876e4fe87a6880883d6f9808fa
Karsten Vileid <kvileid@yahoo.com>
2015-05-27 10:32:20 +0200

    cleanup

--- src/main/java/no/kvileid/jpa/PersistenceUtil.java
+++ src/main/java/no/kvileid/jpa/PersistenceUtil.java
@@ -7,14 +7,15 @@ import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.spi.PersistenceUnitTransactionType;
 
+import org.hibernate.boot.registry.classloading.internal.ClassLoaderServiceImpl;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.ejb.AvailableSettings;
-//import org.hibernate.ejb.Ejb3Configuration;
+import org.hibernate.jpa.AvailableSettings;
+import org.hibernate.jpa.boot.internal.ParsedPersistenceXmlDescriptor;
+import org.hibernate.jpa.boot.internal.PersistenceXmlParser;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 
-
-@SuppressWarnings("deprecation")
 public class PersistenceUtil {
     private static EntityManager entityManager;
 
@@ -25,7 +26,6 @@ public class PersistenceUtil {
 
     public static EntityManager beginTransaction() {
         entityManager.getTransaction().begin();
-
         return entityManager;
     }
 
@@ -34,7 +34,8 @@ public class PersistenceUtil {
     }
 
     public static void closeSession() {
-        // Closing entityManagerFactory required due to bug in Hibernate, see http://stackoverflow.com/questions/21645516/program-using-hibernate-does-not-terminate
+        // Closing entityManagerFactory required due to bug in Hibernate, see
+        // http://stackoverflow.com/questions/21645516/program-using-hibernate-does-not-terminate
         EntityManagerFactory entityManagerFactory = entityManager.getEntityManagerFactory();
         entityManager.close();
         entityManagerFactory.close();
@@ -44,34 +45,31 @@ public class PersistenceUtil {
         entityManager.getTransaction().rollback();
     }
 
-//    private static void recreateDatabase(Class<?>... entityClasses) {
-//        Ejb3Configuration config = new Ejb3Configuration().configure("JpaCert", null);
-//        for (Class<?> clazz : entityClasses) {
-//            config.addAnnotatedClass(clazz);
-//        }
-//        new SchemaExport(config.getHibernateConfiguration()).create(true, true);
-//    }
-
     private static EntityManagerFactory createFactory(Class<?>... entityClasses) {
         Map<String, Object> props = new HashMap<>();
-        props.put(org.hibernate.jpa.AvailableSettings.LOADED_CLASSES, Arrays.asList(entityClasses));
+        props.put(AvailableSettings.LOADED_CLASSES, Arrays.asList(entityClasses));
         return Persistence.createEntityManagerFactory("JpaCert", props);
     }
 
     private static void recreateDatabase(Class<?>... entityClasses) {
-//    PersistenceXmlParser parser = new PersistenceXmlParser(new ClassLoaderServiceImpl(), PersistenceUnitTransactionType.RESOURCE_LOCAL);
-//    ParsedPersistenceXmlDescriptor descriptors = parser.doResolve(new HashMap<>()).get(0);
+        PersistenceXmlParser parser = new PersistenceXmlParser(new ClassLoaderServiceImpl(),
+                PersistenceUnitTransactionType.RESOURCE_LOCAL);
+        ParsedPersistenceXmlDescriptor descriptor = parser.doResolve(new HashMap<>()).get(0);
 
         Configuration cfg = new Configuration();
-        cfg.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
-        cfg.setProperty("hibernate.connection.url", "jdbc:h2:/var/tmp/h2/jpacert;AUTO_SERVER=TRUE");
-        cfg.setProperty("hibernate.connection.username", "sa");
-        cfg.setProperty("hibernate.connection.password", "");
+        cfg.setProperty("hibernate.connection.driver_class", getProperty(descriptor, AvailableSettings.JDBC_DRIVER));
+        cfg.setProperty("hibernate.connection.url", getProperty(descriptor, AvailableSettings.JDBC_URL));
+        cfg.setProperty("hibernate.connection.username", getProperty(descriptor, AvailableSettings.JDBC_USER));
+        cfg.setProperty("hibernate.connection.password", getProperty(descriptor, AvailableSettings.JDBC_PASSWORD));
         cfg.setProperty("hibernate.show-sql", "true");
         cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         for (Class<?> clazz : entityClasses) {
             cfg.addAnnotatedClass(clazz);
         }
         new SchemaExport(cfg).create(true, true);
+    }
+
+    private static String getProperty(ParsedPersistenceXmlDescriptor descriptor, String propertyName) {
+        return descriptor.getProperties().getProperty(propertyName);
     }
 }
